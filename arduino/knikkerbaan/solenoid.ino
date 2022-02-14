be07e4fc280b1e509e2827d69f69897ece7edf92
Olivier Drees <57214689+olvu400@users.noreply.github.com>
2022-02-14 13:57:17 +0100

    Update solenoid.ino

--- arduino/knikkerbaan/solenoid.ino
+++ arduino/knikkerbaan/solenoid.ino
@@ -1,44 +1,18 @@
 // gebruik Grove - Mosfet (Rood > 5V, Zwart > GND, Geel > Pin 6)
 
-int motorPin = 6;
- 
-void setup()
-{
-    Serial.begin(38400);
-    pinMode(motorPin, OUTPUT);
-    Serial.println("Grove - MOSFET Test Demo!");
+
+// the setup function runs once when you press reset or power the board
+int SolenoidPin = 6;
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(SolenoidPin, OUTPUT);
 }
- 
-void loop()
-{
-    motorOnThenOffWithSpeed();
-    motorAcceleration();
-}
- 
-void motorOnThenOffWithSpeed()
-{
-    int onSpeed  = 200;                         // a number between 0 (stopped) and 255 (full speed)
-    int onTime   = 2500;
-    int offSpeed = 50;                          // a number between 0 (stopped) and 255 (full speed)
-    int offTime  = 1000;
-    analogWrite(motorPin, onSpeed);
-    delay(onTime);
-    analogWrite(motorPin, offSpeed);
-    delay(offTime);
-}
- 
-void motorAcceleration()
-{
-    int delayTime = 50;
-    for(int i=0; i<256; i++)
-    {
-        analogWrite(motorPin, i);
-        delay(delayTime);
-    }
- 
-    for(int i=255; i>=0; i--)
-    {
-        analogWrite(motorPin, i);
-        delay(delayTime);
-    }
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(SolenoidPin, HIGH);   
+  delay(1000);                       
+  digitalWrite(SolenoidPin, LOW);    
+  delay(1000);                       
 }
