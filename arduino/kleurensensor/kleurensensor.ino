0b630e1d324c213962232199d3c3adee0f0fbf69
cam-emmauscollege <45658387+cam-emmauscollege@users.noreply.github.com>
2022-02-16 16:31:12 +0100

    Update kleurensensor.ino

--- arduino/kleurensensor/kleurensensor.ino
+++ arduino/kleurensensor/kleurensensor.ino
@@ -1,29 +1,72 @@
 #include <Wire.h>
-#include <GroveColorSensor.h>
+#include "Adafruit_TCS34725.h"
+
+// Pick analog outputs, for the UNO these three work well
+// use ~560  ohm resistor between Red & Blue, ~1K for green (its brighter)
+#define redpin 6
+#define greenpin 10
+#define bluepin 11
+
+// set to false if using a common cathode LED
+#define commonAnode false
+
+// our RGB -> eye-recognized gamma color
+byte gammatable[256];
 
 
-void setup()
-{
-    Serial.begin(9600);
-    Wire.begin();
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
+
+void setup() {
+  Serial.begin(9600);
+
+  if (tcs.begin()) {
+    //Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+
+  // use these three pins to drive an LED
+
+  pinMode(redpin, OUTPUT);
+  pinMode(greenpin, OUTPUT);
+  pinMode(bluepin, OUTPUT);
+
+
+  // thanks PhilB for this gamma table!
+  // it helps convert RGB colors to what humans see
+  for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+
+    if (commonAnode) {
+      gammatable[i] = 255 - x;
+    } else {
+      gammatable[i] = x;
+    }
+  }
 }
 
-void loop()
-{
-    int red, green, blue;
-    GroveColorSensor colorSensor;
-    colorSensor.ledStatus = 1;            // When turn on the color sensor LED, ledStatus = 1; When turn off the color sensor LED, ledStatus = 0.
-    while(1)
-    {
-        colorSensor.readRGB(&red, &green, &blue);    //Read RGB values to variables.
-        delay(300);
-        Serial.print("The RGB value are: RGB( ");
-        Serial.print(red,DEC);
-        Serial.print(", ");
-        Serial.print(green,DEC);
-        Serial.print(", ");
-        Serial.print(blue,DEC);
-        Serial.println(" )");
-        colorSensor.clearInterrupt();
-    }
+
+void loop() {
+  float red, green, blue;
+  
+  tcs.setInterrupt(false);  // turn on LED
+
+  delay(60);  // takes 50ms to read
+
+  tcs.getRGB(&red, &green, &blue);
+  
+  tcs.setInterrupt(true);  // turn off LED
+
+  Serial.print("R:\t"); Serial.print(int(red)); 
+  Serial.print("\tG:\t"); Serial.print(int(green)); 
+  Serial.print("\tB:\t"); Serial.print(int(blue));
+  Serial.print("\n");
+
+  analogWrite(redpin, gammatable[(int)red]);
+  analogWrite(greenpin, gammatable[(int)green]);
+  analogWrite(bluepin, gammatable[(int)blue]);
 }
