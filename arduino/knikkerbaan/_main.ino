9bbb8520ff1d5b8382aeb73917453b63c39674f2
SanneV <57942385+SanneV@users.noreply.github.com>
2022-02-23 22:19:53 +0100

    Update _main.ino

--- arduino/knikkerbaan/_main.ino
+++ arduino/knikkerbaan/_main.ino
@@ -3,26 +3,31 @@
 
 Servo servo360;
 
-
-
 KnikkerPoort poortBoven = KnikkerPoort();
+Wissel wisselPoort = KnikkerPoort();
 WiFiCommunicator wifi = WiFiCommunicator(WIFI_NETWERK, WIFI_WACHTWOORD, SERVER_DOMEINNAAM);
 Teller tellerA = Teller(TELLER_A_PIN);
 Teller tellerB = Teller(TELLER_B_PIN);
 
-int serverContactInterval = 15;                // 15 seconden
+int serverContactInterval = 15; // 15 seconden
+bool wisselRechts = false; 
 unsigned long tijdVoorContactMetServer = 0;
+unsigned long tijdWisselScheef = 0;
+unsigned long tijdWisselMidden = 0;
+
 
 void setup() {
   Serial.begin(9600);
   
   poortBoven.begin(BOVEN_POORT_PIN, 0, 90);
+  wisselPoort.begin(WISSEL_PIN,0, 90, 45);
   
   //wifi.begin();
 
   //wifi.stuurVerzoek("/api/set/nieuwerun", "");
 
   poortBoven.open();
+  wisselPoort.midden();
 
   servo360.attach(3);
 
@@ -52,7 +57,38 @@ void loop() {
 
     wifi.stuurVerzoek("/api/set/sensordata", data. c_str());
 
+    String serverAntwoord = wifi.stuurVerzoek("/api/get/instellingen", "");
+    Serial.println(serverAntwoord);
+
+    JSONVar ontvangenInstellingen = JSON.parse(serverAntwoord);
+
+    if(JSON.typeof(ontvangenInstellingen != "undefined")){
+      //nieuwe wachttijd instellen:
+      serverContactInterval = (int)ontvangenInstellingen["wachttijdPoort"];
+      wisselRechts = (int)ontvangenInstellingen["wisselStaatRechts"];
+    }
+    else{
+      Serial.println("FOUT: serverAntwoord kon niet worden verwerkt");
+    }
+
     tijdVoorContactMetServer = millis() + (unsigned long)serverContactInterval * 1000;
+
+    if(wisselRechts = true && staatMidden = true){
+      wisselPoort.rechts();
+      tijdWisselScheef = millis() + 3000;
+      if(millis() > tijdWisselScheef && !wisselPoort.getMidden){
+        wisselPoort.midden();
+      }
+    }
+    
+    if(wisselRechts = false && staatMidden = true){
+      wisselPoort.links();
+      tijdWisselScheef = millis() + 3000;
+      if(millis() > tijdWisselScheef && !wisselPoort.getMidden){
+        wisselPoort.midden();
+      }
+    }
+      
     // en zet nu het poortje weer open:
     poortBoven.open();
 
