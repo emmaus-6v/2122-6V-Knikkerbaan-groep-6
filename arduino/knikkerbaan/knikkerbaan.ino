7b58c2beef21f1b0a629e856265cafe66abc1dc5
SanneV <57942385+SanneV@users.noreply.github.com>
2022-02-23 20:47:05 +0100

    Update knikkerbaan.ino

--- arduino/knikkerbaan/knikkerbaan.ino
+++ arduino/knikkerbaan/knikkerbaan.ino
@@ -3,7 +3,7 @@
 //declareer hier je globale constanten:
 
 const int BOVEN_POORT_PIN = 9;    // pin van servo die bovenste poort regelt
-const int BOVEN_POORT_PIN = 10;    // pin van servo die de wissel regelt
+const int WISSEL_PIN = 10;    // pin van servo die de wissel regelt
 const int LEEGLOOP_TIJD = 5000;   // tijd die knikkers nodig hebben om na het
                                   // sluiten van de poort langs alle sensors te komen
 
