707da793ed9c66e9ca035ce0b17ac9329ef69902
SanneV <57942385+SanneV@users.noreply.github.com>
2022-02-23 16:43:04 +0100

    Update _main.ino

--- arduino/knikkerbaan/_main.ino
+++ arduino/knikkerbaan/_main.ino
@@ -8,6 +8,7 @@ Servo myservo;
 KnikkerPoort poortBoven = KnikkerPoort();
 WiFiCommunicator wifi = WiFiCommunicator(WIFI_NETWERK, WIFI_WACHTWOORD, SERVER_DOMEINNAAM);
 Teller tellerA = Teller(TELLER_A_PIN);
+Teller tellerB = Teller(TELLER_B_PIN);
 
 int serverContactInterval = 15;                // 15 seconden
 unsigned long tijdVoorContactMetServer = 0;
@@ -17,9 +18,9 @@ void setup() {
   
   poortBoven.begin(BOVEN_POORT_PIN, 0, 90);
   
-  wifi.begin();
+  //wifi.begin();
 
-  wifi.stuurVerzoek("/api/set/nieuwerun", "");
+  //wifi.stuurVerzoek("/api/set/nieuwerun", "");
 
   poortBoven.open();
 
@@ -41,8 +42,10 @@ void loop() {
   // knikkerbaan is leeggelopen, er zijn geen sensors dit iets moeten meten
   // nu is het tijd om contact te leggen met de server:
   if (millis() > tijdVoorContactMetServer + LEEGLOOP_TIJD) {
-    Serial.print("Er zijn nu zoveel knikkers geteld: ");
+    Serial.print("Er zijn nu zoveel knikkers geteld bij sensor 1: ");
     Serial.println(tellerA.getAantal());
+    Serial.print("Er zijn nu zoveel knikkers geteld bij sensor 2: ");
+    Serial.println(tellerB.getAantal());
 
     String data = "knikkers=";
     data += tellerA.getAantal();
