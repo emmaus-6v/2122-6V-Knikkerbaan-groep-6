554e8faf59f6a7c8ee757c24b8cfdedf66e429e2
SanneV <57942385+SanneV@users.noreply.github.com>
2022-02-23 20:46:04 +0100

    Update knikkerbaan.ino

--- arduino/knikkerbaan/knikkerbaan.ino
+++ arduino/knikkerbaan/knikkerbaan.ino
@@ -3,6 +3,7 @@
 //declareer hier je globale constanten:
 
 const int BOVEN_POORT_PIN = 9;    // pin van servo die bovenste poort regelt
+const int BOVEN_POORT_PIN = 10;    // pin van servo die de wissel regelt
 const int LEEGLOOP_TIJD = 5000;   // tijd die knikkers nodig hebben om na het
                                   // sluiten van de poort langs alle sensors te komen
 
@@ -11,4 +12,4 @@ char WIFI_WACHTWOORD[] = "informatica-rocks!";
 char SERVER_DOMEINNAAM[] = "";    // domeinnaam van gitpod-server, zoals 3000-lavendel-bla-bla.gitpod.io
 
 const int TELLER_A_PIN = 5;       // pin waarop IR-sensor voor Teller A is aangesloten
-const int TELLER_B_PIN = 10;       // pin waarop IR-sensor voor Teller B is aangesloten
+const int TELLER_B_PIN = 11;       // pin waarop IR-sensor voor Teller B is aangesloten
