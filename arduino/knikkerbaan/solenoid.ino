3d12b8caadf74444b1e7b4276e0407a80c7f2e33
SanneV <57942385+SanneV@users.noreply.github.com>
2022-02-24 20:59:39 +0100

    Update solenoid.ino

--- arduino/knikkerbaan/solenoid.ino
+++ arduino/knikkerbaan/solenoid.ino
@@ -1,59 +1,32 @@
 // gebruik Grove - Mosfet (Rood > 5V, Zwart > GND, Geel > Pin 6)
 
-
-// the setup function runs once when you press reset or power the board
-/*int SolenoidPin = 6;
-
-void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(SolenoidPin, OUTPUT);
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  digitalWrite(SolenoidPin, HIGH);   
-  delay(1000);                       
-  digitalWrite(SolenoidPin, LOW);    
-  delay(10000);                       
-}*/
-
 class Solenoid{
   private:
   int pin;
-  bool solenoidHigh = true;
+  bool solenoidStatus = LOW;
   unsigned long solenoidTijd = 0;
 
   public:
-  Solenoid(){}
-
   Solenoid(int _pin) {
-    pinMode(pin, OUTPUT);
     pin = _pin;
-    
+    pinMode(pin, OUTPUT);
   }
 
   void update(){
-    if(millis() > solenoidTijd){
-      if(solenoidHigh == false){
-        solenoidHigh = true;
-        digitalWrite(pin, HIGH);
-        solenoidTijd = millis() + 5000;
+    unsigned long huidigeTijd = millis();
+    
+    if(huidigeTijd > solenoidTijd){
+      if(solenoidStatus == LOW){
+        solenoidStatus = HIGH;
+        solenoidTijd = huidigeTijd + 10000;
       }
-      if(solenoidHigh == true){
-        solenoidHigh = false;
-        digitalWrite(pin, LOW);
-        solenoidTijd = millis() + 5000;
+      if(solenoidStatus == HIGH){
+        solenoidStatus = LOW;
+        solenoidTijd = huidigeTijd + 1000;
       }
+      digitalWrite(pin, solenoidStatus);
     }
   }
   
 };
 
-Solenoid solenoid = Solenoid(7);
- void setup(){
-  
-  }
-
-  void loop(){
-    solenoid.update();
-  }
