3cce313c5f54dc2b928ed796f5f1e67c6172e27a
SanneV <57942385+SanneV@users.noreply.github.com>
2022-02-24 17:53:47 +0100

    proberen classe van te maken (nog niet klaar)

--- arduino/knikkerbaan/solenoid.ino
+++ arduino/knikkerbaan/solenoid.ino
@@ -2,7 +2,7 @@
 
 
 // the setup function runs once when you press reset or power the board
-int SolenoidPin = 6;
+/*int SolenoidPin = 6;
 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
@@ -15,4 +15,45 @@ void loop() {
   delay(1000);                       
   digitalWrite(SolenoidPin, LOW);    
   delay(10000);                       
-}
+}*/
+
+class Solenoid{
+  private:
+  int pin;
+  bool solenoidHigh = true;
+  unsigned long solenoidTijd = 0;
+
+  public:
+  Solenoid(){}
+
+  Solenoid(int _pin) {
+    pinMode(pin, OUTPUT);
+    pin = _pin;
+    
+  }
+
+  void update(){
+    if(millis() > solenoidTijd){
+      if(solenoidHigh == false){
+        solenoidHigh = true;
+        digitalWrite(pin, HIGH);
+        solenoidTijd = millis() + 5000;
+      }
+      if(solenoidHigh == true){
+        solenoidHigh = false;
+        digitalWrite(pin, LOW);
+        solenoidTijd = millis() + 5000;
+      }
+    }
+  }
+  
+};
+
+Solenoid solenoid = Solenoid(7);
+ void setup(){
+  
+  }
+
+  void loop(){
+    solenoid.update();
+  }
